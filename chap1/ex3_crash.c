7c7c1f00d71fdc2b10c5dae05e221b41bfaa2295
rlatjdrud <jkkim1933@naver.com>
2023-02-10 16:40:23 +0900

    chap1 debugging

---
+++ chap1/ex3_crash.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void crash(){
+	char *text =NULL;
+	int i =0;
+
+	for(i=0; i<1000000; i++){
+	printf("%c",text[i]);
+	}
+}
+
+int main()
+{
+	int age=10;
+	int height=72;
+	
+	printf("i am %d inches tall.\n",height);
+	crash();
+	crash();
+	crash();
+	crash();
+	crash();
+	crash();
+
+	return 0;
+}
