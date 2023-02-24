dea73df1a8af1250558bce552904df19d8c50306
rlatjdrud <jkkim1933@naver.com>
2023-02-24 14:36:31 +0900

    ex017와 stdio 추가

---
+++ stdio_file/getchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	int i;
+	char c;
+
+	scanf("%d",&i);
+	scanf("%c",&c);
+	
+	printf("입력한 문자 : %c\n",c);
+
+	return 0;
+	
+}
---
+++ stdio_file/stdin.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	int num;
+	char word;
+	char name[10];
+
+	num=getchar();
+	printf("%c",num);
+
+	return 0;
+
+}
